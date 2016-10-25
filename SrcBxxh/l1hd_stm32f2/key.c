d35493ffec60ab62903718f5b4d304c49fedd975
smdzjl@sina.cn <smdzjl@sina.cn>
2016-10-25 21:14:17 +0800

    zjl@2016/10/25 R03.039 Update

--- SrcBxxh/l1hd_stm32f2/key.c
+++ SrcBxxh/l1hd_stm32f2/key.c
@@ -1,4 +1,4 @@
-﻿/*----------------------------------------Copyright (c)---------------------------------------------------                              
+/*----------------------------------------Copyright (c)---------------------------------------------------                              
 *                                        http://www.39board.com
 *
 *      39board工作室提供的所有ST、TI、Freescale、Altera等平台的例程、文档、方案等资料信息旨在为学习者提供
@@ -21,7 +21,6 @@
 
 #include "stm32f2xx.h"
 #include "key.h"
-
 
 /*
 *--------------------------------------------------------------------------------------------------------
@@ -70,4 +69,81 @@ u8  get_key_state (void)
 
 
 
-/************************ (C) COPYRIGHT 39BOARD *****************************END OF FILE********************************/
+
+
+/*******************************************************************************
+* 函数名  : Key_GPIO_Config
+* 描述    : KEY IO配置
+* 输入    : 无
+* 输出    : 无
+* 返回    : 无 
+* 说明    : KEY(1~2)的IO口分别是:PC10,PC11
+*******************************************************************************/
+void Key_GPIO_Config(void)
+{
+	GPIO_InitTypeDef  GPIO_InitStructure;				//定义一个GPIO_InitTypeDef类型的GPIO初始化结构体
+	
+	RCC_APB2PeriphClockCmd(KEY_RCC, ENABLE);			//使能GPIOA的外设时钟	
+	
+	GPIO_InitStructure.GPIO_Pin = KEY1 | KEY2;			//选择要初始化的GPIOA引脚(PA15,PA14)
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;		//设置引脚工作模式为上拉输入 		
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	//设置引脚输出最大速率为50MHz
+	GPIO_Init(KEY_PORT, &GPIO_InitStructure);			//调用库函数中的GPIO初始化函数，初始化GPIOA中的PA15,PA14引脚
+}
+
+/*******************************************************************************
+* 函数名  : Key_Detection
+* 描述    : 按键按下检测
+* 输入    : GPIOx：按键对应的GPIO，GPIO_Pin：对应按键端口
+* 输出    : 无
+* 返回    : KEY_DOWN(0):对应按键按下，KEY_UP(1):对应按键没按下
+* 说明    : KEY(1~2)的IO口分别是:PC10,PC11
+*******************************************************************************/
+u8 Key_Down_Detection(GPIO_TypeDef* GPIOx, u16 GPIO_Pin)
+{			
+	if(GPIO_ReadInputDataBit(GPIOx,GPIO_Pin) == KEY_DOWN)	//检测是否有按键按下 
+	{	   
+		//Delay_nMs(10);	//延时消抖		
+		if(GPIO_ReadInputDataBit(GPIOx,GPIO_Pin) == KEY_DOWN)	//检测是否有按键按下   
+		{	 
+			while(GPIO_ReadInputDataBit(GPIOx,GPIO_Pin) == KEY_DOWN);	//等待按键释放  
+			return KEY_DOWN;	 
+		}
+		else
+		{
+			return KEY_UP;
+		}
+	}
+	else
+	{
+		return KEY_UP;
+	}
+}
+
+/*******************************************************************************
+* 函数名  : Key_Down_Scan
+* 描述    : 按键按下扫描
+* 输入    : 无
+* 输出    : 0：没有按键按下，1：按键按下
+* 返回    : 无 
+* 说明    : KEY(1~2)的IO口分别是:PC10,PC11
+*******************************************************************************/
+u8 Key_Down_Scan(void)
+{
+	u16 downflag=0;
+	u8 value=0;
+
+  	downflag=GPIO_ReadInputData(KEY_PORT);
+	if((downflag & 0x0c00) != 0x0c00)	//检测是否有按键按下 
+	{	   
+		//Delay_nMs(10);	//延时消抖	
+		downflag = GPIO_ReadInputData(KEY_PORT) & 0x0c00;	
+		if(downflag != 0x0c00 )	//检测是否有按键按下   
+		{	 
+			if((downflag & 0x0400) == 0) value |= 0x01;
+			if((downflag & 0x0800) == 0) value |= 0x02; 	 
+		}		
+	}	
+
+	return value;
+}
--- SrcBxxh/l1hd_stm32f2/key.h
+++ SrcBxxh/l1hd_stm32f2/key.h
@@ -43,6 +43,22 @@ void  key_init (void);
 
 u8  get_key_state (void);
 
+//从另外一个例程中得到的程序
+//KEY端口定义
+#define KEY_RCC     RCC_AHB1Periph_GPIOC                            
+#define KEY_PORT	GPIOC   
+#define KEY1        GPIO_Pin_10    
+#define KEY2        GPIO_Pin_11    
+
+//按键标置
+#define KEY_DOWN	0
+#define KEY_UP		1
+
+void Key_GPIO_Config(void);
+u8 Key_Down_Detection(GPIO_TypeDef* GPIOx, u16 GPIO_Pin);
+u8 Key_Down_Scan(void);
+
+	
 #ifdef __cplusplus
 }
 #endif
