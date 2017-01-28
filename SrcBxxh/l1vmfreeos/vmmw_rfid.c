8098b683929681ef34b3e12381db166be298b931
smdzjl@sina.cn <smdzjl@sina.cn>
2017-01-28 21:12:24 +0800

    zjl@2017/1/28 R03.101

--- SrcBxxh/l1vmfreeos/vmmw_rfid.c
+++ SrcBxxh/l1vmfreeos/vmmw_rfid.c
@@ -180,7 +180,24 @@ OPSTAT ihu_vmmw_rfidmod_rc522_spi_read_id(uint8_t *rfidAddr, uint8_t len)
 	
 	//设置工作方式	
 	M500PcdConfigISOType('A');
-	
+
+	uint8_t * UID;
+	uint8_t * KEY;
+	uint8_t * Dat;
+	*UID = 1;
+	*KEY = 0;
+	*Dat = 0x01;
+	*(Dat+1) = 0x02;
+	*(Dat+2) = 0x03;
+	*(Dat+3) = 0x04;
+	//测试写入
+	PcdRequest ( 0x52, ucArray_ID );//寻卡
+  PcdAnticoll ( ucArray_ID );//防冲撞
+  PcdSelect ( UID );//选定卡
+  PcdAuthState ( 0x60, 0x10, KEY, UID );//校验
+  PcdWrite ( 0x10, Dat );
+  PcdRead ( 0x10, Dat );
+
 	//循环读取
 	res = IHU_FAILURE;
 	while((tickTotal > 0) && (res == IHU_FAILURE))
@@ -190,16 +207,19 @@ OPSTAT ihu_vmmw_rfidmod_rc522_spi_read_id(uint8_t *rfidAddr, uint8_t len)
     /*寻卡*/
 		if ( ( ucStatusReturn = PcdRequest ( PICC_REQALL, ucArray_ID ) ) != MI_OK )                                   
 			 /*若失败再次寻卡*/
-      ucStatusReturn = PcdRequest ( PICC_REQALL, ucArray_ID );		   
+      ucStatusReturn = PcdRequest ( PICC_REQALL, ucArray_ID );		
+		IhuDebugPrint("VMMWRFID: Step1 reach.\n");
 		if ( ucStatusReturn == MI_OK  )
 		{
       /*防冲撞（当有多张卡进入读写器操作范围时，防冲突机制会从其中选择一张进行操作）*/
+			IhuDebugPrint("VMMWRFID: Step2 reach.\n");
 			if ( PcdAnticoll(ucArray_ID) == MI_OK )  
 			{
 				memcpy(rfidAddr, ucArray_ID, 4);
 				sprintf(cStr, "The Card ID is: %02X%02X%02X%02X", ucArray_ID[0], ucArray_ID[1], ucArray_ID[2], ucArray_ID[3] );							
 				IhuDebugPrint("VMMWRFID: [%s]\n", cStr);
 				res = IHU_SUCCESS;
+				PcdHalt (); 		
       }
 		}
 	}
@@ -233,92 +253,6 @@ OPSTAT ihu_vmmw_rfidmod_rc522_spi_read_id(uint8_t *rfidAddr, uint8_t len)
 }
 
 
-
-
-/* 扩展变量 ------------------------------------------------------------------*/
-//extern SPI_HandleTypeDef hspiflash;
-/* 私有函数原形 --------------------------------------------------------------*/
-
-///*
-// * 函数名：SPI_RC522_SendByte
-// * 描述  ：向RC522发送1 Byte 数据
-// * 输入  ：byte，要发送的数据
-// * 返回  : RC522返回的数据
-// * 调用  ：内部调用
-// */
-//void SPI_RC522_SendByte ( uint8_t byte )
-//{
-//    uint8_t counter;
-//	
-//	
-//    for(counter=0;counter<8;counter++)
-//    {     
-//			if ( byte & 0x80 )
-//					macRC522_MOSI_1() ;
-//			else 
-//					macRC522_MOSI_0() ;
-
-////			Delay_us ( 3 );
-//			macRC522_DELAY();
-//		
-//			macRC522_SCK_0() ;
-
-////			Delay_us ( 1 );
-////			Delay_us ( 3 );
-//			macRC522_DELAY();
-//			 
-//			macRC522_SCK_1();
-
-////			Delay_us ( 3 );
-//			macRC522_DELAY();
-//			 
-//			byte <<= 1; 
-//			
-//    } 
-//	
-//}
-
-///*
-// * 函数名：SPI_RC522_ReadByte
-// * 描述  ：从RC522发送1 Byte 数据
-// * 输入  ：无
-// * 返回  : RC522返回的数据
-// * 调用  ：内部调用
-// */
-//uint8_t SPI_RC522_ReadByte ( void )
-//{
-//	uint8_t counter;
-//	uint8_t SPI_Data;
-
-
-//	for(counter=0;counter<8;counter++)
-//	{
-//			SPI_Data <<= 1;
-//	 
-//			macRC522_SCK_0() ;
-
-////			Delay_us ( 3 );
-//		macRC522_DELAY();
-//		
-//			if ( macRC522_MISO_GET() == 1)
-//					SPI_Data |= 0x01;
-
-////			Delay_us ( 2 );
-////			Delay_us ( 3 );
-//			macRC522_DELAY();
-
-//			macRC522_SCK_1() ;
-//	
-////			Delay_us ( 3 );
-//			macRC522_DELAY();
-//			
-//	}
-//	
-//	return SPI_Data;
-//	
-//}
-
-
 /*
  * 函数名：ReadRawRC
  * 描述  ：读RC522寄存器
@@ -330,21 +264,12 @@ uint8_t ReadRawRC ( uint8_t ucAddress )
 {
 	uint8_t ucAddr, ucReturn;
 	
-	
 	ucAddr = ( ( ucAddress << 1 ) & 0x7E ) | 0x80;
-	
 	macRC522_CS_Enable() ;
-	
 	SPI_FLASH_SendByte ( ucAddr );
-	
 	ucReturn = SPI_FLASH_ReadByte ();
-	
 	macRC522_CS_Disable();
-	
-	
 	return ucReturn;
-	
-	
 }
 
 /*
@@ -359,18 +284,11 @@ void WriteRawRC ( uint8_t ucAddress, uint8_t ucValue )
 {  
 	uint8_t ucAddr;
 	
-	
 	ucAddr = ( ucAddress << 1 ) & 0x7E;
-	
 	macRC522_CS_Enable() ;
-	
 	SPI_FLASH_SendByte ( ucAddr );
-	
 	SPI_FLASH_SendByte ( ucValue );
-	
 	macRC522_CS_Disable();	
-
-	
 }
 
 /*
@@ -385,12 +303,8 @@ void SetBitMask ( uint8_t ucReg, uint8_t ucMask )
 {
     uint8_t ucTemp;
 	
-	
     ucTemp = ReadRawRC ( ucReg );
-	
     WriteRawRC ( ucReg, ucTemp | ucMask );         // set bit mask
-	
-	
 }
 
 /*
@@ -405,12 +319,8 @@ void ClearBitMask ( uint8_t ucReg, uint8_t ucMask )
 {
     uint8_t ucTemp;
 	
-	
     ucTemp = ReadRawRC ( ucReg );
-	
     WriteRawRC ( ucReg, ucTemp & ( ~ ucMask) );  // clear bit mask
-	
-	
 }
 
 /*
@@ -424,13 +334,9 @@ void PcdAntennaOn ( void )
 {
     uint8_t uc;
 	
-	
     uc = ReadRawRC ( TxControlReg );
-	
     if ( ! ( uc & 0x03 ) )
 			SetBitMask(TxControlReg, 0x03);
-
-		
 }
 
 
@@ -456,34 +362,23 @@ void PcdAntennaOff ( void )
 void PcdReset ( void )
 {
 	macRC522_Reset_Disable();
-	
 	HAL_Delay ( 1 );
-	
 	macRC522_Reset_Enable();
-	
 	HAL_Delay ( 1 );
-	
 	macRC522_Reset_Disable();
-	
 	HAL_Delay ( 1 );
-	
 	WriteRawRC ( CommandReg, 0x0f );
-	
 	while ( ReadRawRC ( CommandReg ) & 0x10 );
-	
 	HAL_Delay ( 1 );
-	
   WriteRawRC ( ModeReg, 0x3D );            //定义发送和接收常用模式 和Mifare卡通讯，CRC初始值0x6363
-	
   WriteRawRC ( TReloadRegL, 30 );          //16位定时器低位    
 	WriteRawRC ( TReloadRegH, 0 );			     //16位定时器高位
-	
   WriteRawRC ( TModeReg, 0x8D );				   //定义内部定时器的设置
-	
   WriteRawRC ( TPrescalerReg, 0x3E );			 //设置定时器分频系数
-	
-	WriteRawRC ( TxAutoReg, 0x40 );				   //调制发送信号为100%ASK	
-	
+	WriteRawRC ( TxAutoReg, 0x40 );				   //调制发送信号为100%ASK
+	PcdAntennaOff();
+  HAL_Delay ( 1 );
+  PcdAntennaOn();
 }
 
 /*
@@ -498,27 +393,16 @@ void M500PcdConfigISOType ( uint8_t ucType )
 	if ( ucType == 'A')                     //ISO14443_A
   {
 		ClearBitMask ( Status2Reg, 0x08 );
-		
     WriteRawRC ( ModeReg, 0x3D );//3F
-		
 		WriteRawRC ( RxSelReg, 0x86 );//84
-		
 		WriteRawRC( RFCfgReg, 0x7F );   //4F
-		
 		WriteRawRC( TReloadRegL, 30 );//tmoLength);// TReloadVal = 'h6a =tmoLength(dec) 
-		
 		WriteRawRC ( TReloadRegH, 0 );
-		
 		WriteRawRC ( TModeReg, 0x8D );
-		
 		WriteRawRC ( TPrescalerReg, 0x3E );
-		
 		HAL_Delay ( 2 );
-		
 		PcdAntennaOn ();//开天线
-		
    }
- 
 }
 
 /*
@@ -542,92 +426,64 @@ char PcdComMF522 ( uint8_t ucCommand, uint8_t * pInData, uint8_t ucInLenByte, ui
     uint8_t ucN;
     uint32_t ul;
 	
-	
     switch ( ucCommand )
     {
        case PCD_AUTHENT:		//Mifare认证
           ucIrqEn   = 0x12;		//允许错误中断请求ErrIEn  允许空闲中断IdleIEn
           ucWaitFor = 0x10;		//认证寻卡等待时候 查询空闲中断标志位
           break;
-			 
        case PCD_TRANSCEIVE:		//接收发送 发送接收
           ucIrqEn   = 0x77;		//允许TxIEn RxIEn IdleIEn LoAlertIEn ErrIEn TimerIEn
           ucWaitFor = 0x30;		//寻卡等待时候 查询接收中断标志位与 空闲中断标志位
           break;
-			 
        default:
          break;
-			 
     }
-   
     WriteRawRC ( ComIEnReg, ucIrqEn | 0x80 );		//IRqInv置位管脚IRQ与Status1Reg的IRq位的值相反 
     ClearBitMask ( ComIrqReg, 0x80 );			//Set1该位清零时，CommIRqReg的屏蔽位清零
     WriteRawRC ( CommandReg, PCD_IDLE );		//写空闲命令
     SetBitMask ( FIFOLevelReg, 0x80 );			//置位FlushBuffer清除内部FIFO的读和写指针以及ErrReg的BufferOvfl标志位被清除
-    
     for ( ul = 0; ul < ucInLenByte; ul ++ )
 		  WriteRawRC ( FIFODataReg, pInData [ ul ] );    		//写数据进FIFOdata
-			
     WriteRawRC ( CommandReg, ucCommand );					//写命令
-   
-    
     if ( ucCommand == PCD_TRANSCEIVE )
 			SetBitMask(BitFramingReg,0x80);  				//StartSend置位启动数据发送 该位与收发命令使用时才有效
-    
     ul = 1000;//根据时钟频率调整，操作M1卡最大等待时间25ms
-		
     do 														//认证 与寻卡等待时间	
     {
          ucN = ReadRawRC ( ComIrqReg );							//查询事件中断
          ul --;
     } while ( ( ul != 0 ) && ( ! ( ucN & 0x01 ) ) && ( ! ( ucN & ucWaitFor ) ) );		//退出条件i=0,定时器中断，与写空闲命令
-		
     ClearBitMask ( BitFramingReg, 0x80 );					//清理允许StartSend位
-		
     if ( ul != 0 )
     {
 			if ( ! ( ReadRawRC ( ErrorReg ) & 0x1B ) )			//读错误标志寄存器BufferOfI CollErr ParityErr ProtocolErr
 			{
 				cStatus = MI_OK;
-				
 				if ( ucN & ucIrqEn & 0x01 )					//是否发生定时器中断
 				  cStatus = MI_NOTAGERR;   
-					
 				if ( ucCommand == PCD_TRANSCEIVE )
 				{
 					ucN = ReadRawRC ( FIFOLevelReg );			//读FIFO中保存的字节数
-					
 					ucLastBits = ReadRawRC ( ControlReg ) & 0x07;	//最后接收到得字节的有效位数
-					
 					if ( ucLastBits )
 						* pOutLenBit = ( ucN - 1 ) * 8 + ucLastBits;   	//N个字节数减去1（最后一个字节）+最后一位的位数 读取到的数据总位数
 					else
 						* pOutLenBit = ucN * 8;   					//最后接收到的字节整个字节有效
-					
 					if ( ucN == 0 )		
             ucN = 1;    
-					
 					if ( ucN > MAXRLEN )
 						ucN = MAXRLEN;   
-					
 					for ( ul = 0; ul < ucN; ul ++ )
 					  pOutData [ ul ] = ReadRawRC ( FIFODataReg );   
-					
 					}
-					
       }
-			
 			else
 				cStatus = MI_ERR;   
-			
     }
-   
    SetBitMask ( ControlReg, 0x80 );           // stop timer now
    WriteRawRC ( CommandReg, PCD_IDLE ); 
-		 
-		
    return cStatus;
-				
 }
 
 /*
@@ -652,27 +508,19 @@ char PcdRequest ( uint8_t ucReq_code, uint8_t * pTagType )
 	 uint8_t ucComMF522Buf [ MAXRLEN ]; 
    uint32_t ulLen;
 	
-
    ClearBitMask ( Status2Reg, 0x08 );	//清理指示MIFARECyptol单元接通以及所有卡的数据通信被加密的情况
    WriteRawRC ( BitFramingReg, 0x07 );	//	发送的最后一个字节的 七位
    SetBitMask ( TxControlReg, 0x03 );	//TX1,TX2管脚的输出信号传递经发送调制的13.56的能量载波信号
-
    ucComMF522Buf [ 0 ] = ucReq_code;		//存入 卡片命令字
-
    cStatus = PcdComMF522 ( PCD_TRANSCEIVE,	ucComMF522Buf, 1, ucComMF522Buf, & ulLen );	//寻卡  
-  
    if ( ( cStatus == MI_OK ) && ( ulLen == 0x10 ) )	//寻卡成功返回卡类型 
    {    
        * pTagType = ucComMF522Buf [ 0 ];
        * ( pTagType + 1 ) = ucComMF522Buf [ 1 ];
    }
-	 
    else
      cStatus = MI_ERR;
-
-   
    return cStatus;
-	 	 
 }
 
 /*
@@ -689,17 +537,13 @@ char PcdAnticoll ( uint8_t * pSnr )
     uint8_t uc, ucSnr_check = 0;
     uint8_t ucComMF522Buf [ MAXRLEN ]; 
 	  uint32_t ulLen;
-    
-
+ 
     ClearBitMask ( Status2Reg, 0x08 );		//清MFCryptol On位 只有成功执行MFAuthent命令后，该位才能置位
     WriteRawRC ( BitFramingReg, 0x00);		//清理寄存器 停止收发
-    ClearBitMask ( CollReg, 0x80 );			//清ValuesAfterColl所有接收的位在冲突后被清除
-   
-    ucComMF522Buf [ 0 ] = 0x93;	//卡片防冲突命令
+    ClearBitMask ( CollReg, 0x80 );			  //清ValuesAfterColl所有接收的位在冲突后被清除
+    ucComMF522Buf [ 0 ] = PICC_ANTICOLL1;	//卡片防冲突命令
     ucComMF522Buf [ 1 ] = 0x20;
-   
     cStatus = PcdComMF522 ( PCD_TRANSCEIVE, ucComMF522Buf, 2, ucComMF522Buf, & ulLen);//与卡片通信
-	
     if ( cStatus == MI_OK)		//通信成功
     {
 			for ( uc = 0; uc < 4; uc ++ )
@@ -707,17 +551,11 @@ char PcdAnticoll ( uint8_t * pSnr )
          * ( pSnr + uc )  = ucComMF522Buf [ uc ];			//读出UID
          ucSnr_check ^= ucComMF522Buf [ uc ];
       }
-			
       if ( ucSnr_check != ucComMF522Buf [ uc ] )
 				cStatus = MI_ERR;    
-				 
     }
-    
-    SetBitMask ( CollReg, 0x80 );
-		
-		
+    SetBitMask ( CollReg, 0x80 );		
     return cStatus;
-			
 }
 
 /*
@@ -733,20 +571,14 @@ void CalulateCRC ( uint8_t * pIndata, uint8_t ucLen, uint8_t * pOutData )
 {
     uint8_t uc, ucN;
 	
-	
     ClearBitMask(DivIrqReg,0x04);
-	
-    WriteRawRC(CommandReg,PCD_IDLE);
-	
+    WriteRawRC(CommandReg, PCD_IDLE);
     SetBitMask(FIFOLevelReg,0x80);
-	
     for ( uc = 0; uc < ucLen; uc ++)
 	    WriteRawRC ( FIFODataReg, * ( pIndata + uc ) );   
 
     WriteRawRC ( CommandReg, PCD_CALCCRC );
-	
     uc = 0xFF;
-	
     do 
     {
         ucN = ReadRawRC ( DivIrqReg );
@@ -755,7 +587,6 @@ void CalulateCRC ( uint8_t * pIndata, uint8_t ucLen, uint8_t * pOutData )
 		
     pOutData [ 0 ] = ReadRawRC ( CRCResultRegL );
     pOutData [ 1 ] = ReadRawRC ( CRCResultRegM );
-			
 }
 
 /*
@@ -773,29 +604,21 @@ char PcdSelect ( uint8_t * pSnr )
 	  uint8_t ucComMF522Buf [ MAXRLEN ]; 
     uint32_t  ulLen;
     
-    
     ucComMF522Buf [ 0 ] = PICC_ANTICOLL1;
     ucComMF522Buf [ 1 ] = 0x70;
     ucComMF522Buf [ 6 ] = 0;
-	
     for ( uc = 0; uc < 4; uc ++ )
     {
     	ucComMF522Buf [ uc + 2 ] = * ( pSnr + uc );
     	ucComMF522Buf [ 6 ] ^= * ( pSnr + uc );
     }
-		
     CalulateCRC ( ucComMF522Buf, 7, & ucComMF522Buf [ 7 ] );
-  
     ClearBitMask ( Status2Reg, 0x08 );
-
     ucN = PcdComMF522 ( PCD_TRANSCEIVE, ucComMF522Buf, 9, ucComMF522Buf, & ulLen );
-    
     if ( ( ucN == MI_OK ) && ( ulLen == 0x18 ) )
       ucN = MI_OK;  
     else
       ucN = MI_ERR;    
-
-		
     return ucN;		
 }
 
@@ -818,21 +641,15 @@ char PcdAuthState ( uint8_t ucAuth_mode, uint8_t ucAddr, uint8_t * pKey, uint8_t
 	  uint8_t uc, ucComMF522Buf [ MAXRLEN ];
     uint32_t ulLen;
     
-	
     ucComMF522Buf [ 0 ] = ucAuth_mode;
     ucComMF522Buf [ 1 ] = ucAddr;
-	
     for ( uc = 0; uc < 6; uc ++ )
 	    ucComMF522Buf [ uc + 2 ] = * ( pKey + uc );   
-	
     for ( uc = 0; uc < 6; uc ++ )
 	    ucComMF522Buf [ uc + 8 ] = * ( pSnr + uc );   
-
     cStatus = PcdComMF522 ( PCD_AUTHENT, ucComMF522Buf, 12, ucComMF522Buf, & ulLen );
-	
     if ( ( cStatus != MI_OK ) || ( ! ( ReadRawRC ( Status2Reg ) & 0x08 ) ) )
       cStatus = MI_ERR;   
-    		
     return cStatus;			
 }
 
@@ -851,33 +668,23 @@ char PcdWrite ( uint8_t ucAddr, uint8_t * pData )
 	  uint8_t uc, ucComMF522Buf [ MAXRLEN ];
     uint32_t ulLen;
      
-    
     ucComMF522Buf [ 0 ] = PICC_WRITE;
     ucComMF522Buf [ 1 ] = ucAddr;
-	
     CalulateCRC ( ucComMF522Buf, 2, & ucComMF522Buf [ 2 ] );
- 
     cStatus = PcdComMF522 ( PCD_TRANSCEIVE, ucComMF522Buf, 4, ucComMF522Buf, & ulLen );
-
     if ( ( cStatus != MI_OK ) || ( ulLen != 4 ) || ( ( ucComMF522Buf [ 0 ] & 0x0F ) != 0x0A ) )
       cStatus = MI_ERR;   
-        
     if ( cStatus == MI_OK )
     {
 			//memcpy(ucComMF522Buf, pData, 16);
       for ( uc = 0; uc < 16; uc ++ )
 			  ucComMF522Buf [ uc ] = * ( pData + uc );  
-			
       CalulateCRC ( ucComMF522Buf, 16, & ucComMF522Buf [ 16 ] );
-
       cStatus = PcdComMF522 ( PCD_TRANSCEIVE, ucComMF522Buf, 18, ucComMF522Buf, & ulLen );
-			
 			if ( ( cStatus != MI_OK ) || ( ulLen != 4 ) || ( ( ucComMF522Buf [ 0 ] & 0x0F ) != 0x0A ) )
         cStatus = MI_ERR;   
-			
     } 	
     return cStatus;
-	
 }
 
 /*
@@ -894,21 +701,16 @@ char PcdRead ( uint8_t ucAddr, uint8_t * pData )
     char cStatus;
 	  uint8_t uc, ucComMF522Buf [ MAXRLEN ]; 
     uint32_t ulLen;
-    
 
     ucComMF522Buf [ 0 ] = PICC_READ;
     ucComMF522Buf [ 1 ] = ucAddr;
-	
     CalulateCRC ( ucComMF522Buf, 2, & ucComMF522Buf [ 2 ] );
-   
     cStatus = PcdComMF522 ( PCD_TRANSCEIVE, ucComMF522Buf, 4, ucComMF522Buf, & ulLen );
-	
     if ( ( cStatus == MI_OK ) && ( ulLen == 0x90 ) )
     {
 			for ( uc = 0; uc < 16; uc ++ )
         * ( pData + uc ) = ucComMF522Buf [ uc ];   
     }
-		
     else
       cStatus = MI_ERR;   		
     return cStatus;
@@ -927,37 +729,25 @@ char PcdHalt( void )
 	uint8_t ucComMF522Buf [ MAXRLEN ]; 
 	uint32_t  ulLen;
   
-
   ucComMF522Buf [ 0 ] = PICC_HALT;
   ucComMF522Buf [ 1 ] = 0;
-	
   CalulateCRC ( ucComMF522Buf, 2, & ucComMF522Buf [ 2 ] );
  	PcdComMF522 ( PCD_TRANSCEIVE, ucComMF522Buf, 4, ucComMF522Buf, & ulLen );
-
   return MI_OK;
-	
 }
 
 void IC_CMT ( uint8_t * UID, uint8_t * KEY, uint8_t RW, uint8_t * Dat )
 {
   uint8_t ucArray_ID [ 4 ] = { 0 };//先后存放IC卡的类型和UID(IC卡序列号)
   
-	
   PcdRequest ( 0x52, ucArray_ID );//寻卡
-
   PcdAnticoll ( ucArray_ID );//防冲撞
-  
   PcdSelect ( UID );//选定卡
-  
   PcdAuthState ( 0x60, 0x10, KEY, UID );//校验
-	
-
 	if ( RW )//读写选择，1是读，0是写
     PcdRead ( 0x10, Dat );
-   
    else 
      PcdWrite ( 0x10, Dat );
-
    PcdHalt (); 
 }
 
